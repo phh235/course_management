d7d2248abd38334a27bc8d8727b0c937f0459752
phh235 <hoangaatrox23504@gmail.com>
2024-06-14 19:38:19 +0700

    fix sweet alert

--- src/main/resources/static/assets/js/topic.js
+++ src/main/resources/static/assets/js/topic.js
@@ -3,102 +3,34 @@ function resetForm() {
     document.getElementById('nameInput').value = '';
     document.getElementById('imageInput').value = '';
     document.getElementById('motaInput').value = '';
-
-}
-function postTopic() {
-    let Id = document.getElementById('idInput').value;
-    let name = document.getElementById('nameInput').value;
-    let image = document.getElementById('imageInput').value;
-    let description =  document.getElementById('motaInput').value;
-
-    let insertTopic = {}
-    if (Id) {
-        insertTopic = {
-            id: Id,
-            name: name,
-            image: image,
-            description: description
-        };
-    } else {
-        insertTopic = {
-            name: name,
-            image: image,
-            description: description
-        };
-    }
-    console.table(insertTopic)
-    axios.post('/topic-api/postTopic', insertTopic)
-        .then(response => {
-            if (!document.getElementById('idInput').value) {
-                if (response.data.success) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Thêm chuyên đề thành công',
-                        text: 'Thông tin chuyên đề đã được thêm thành công.',
-                        timer: 1500
-                    }).then(() => {
-                        location.reload();
-                    });
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Thêm chuyên đề thất bại',
-                        text: 'Đã xảy ra lỗi khi thêm thông tin chuyên đề. Vui lòng thử lại sau.',
-                    });
-                }
-            } else {
-                if (response.data.success) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Cập nhật thông tin thành công',
-                        text: 'Thông tin chuyên đề đã cập nhật thành công.',
-                    }).then(() => {
-                        location.reload();
-                    });
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Cập nhật thông tin thất bại',
-                        text: 'Đã xảy ra lỗi khi cập nhật thông tin chuyên đề. Vui lòng thử lại sau.',
-                    });
-                }
-            }
-
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Lỗi',
-                text: 'Đã xảy ra lỗi khi gửi yêu cầu. Vui lòng thử lại sau.',
-            });
-        });
 }
 
 function deleteTopic(button) {
     var id = button.getAttribute("data");
     console.log('Topic: ' + id)
-    console.log(button)
     Swal.fire({
-        title: 'Bạn có chắc chắn muốn xóa người dùng này?',
+        title: 'Bạn có chắc chắn xóa không?',
+        text: 'Bạn sẽ không thể hoàn tác',
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3b71ca',
+        confirmButtonColor: '#139c49',
         cancelButtonColor: '#dc4c64',
-        confirmButtonText: 'OK',
+        confirmButtonText: 'Có, xóa nó',
         cancelButtonText: 'Hủy'
     }).then((result) => {
         if (result.isConfirmed) {
             axios.delete(`/topic-api/deleteByIdTopic/${id}`)
                 .then(response => {
                     if (response.data.success) {
-                        Swal.fire(
-                            'Đã xóa!',
-                            'Chuyên đề đã được xóa.',
-                            'success'
-                        ).then(() => {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Thành công',
+                            text: 'Đã xóa chuyên đề.',
+                            timer: 2000,
+                            confirmButtonColor: '#3b71ca'
+                        }).then(() => {
                             location.reload();
-                        });
+                        })
                     } else {
                         Swal.fire(
                             'Lỗi!',
@@ -117,6 +49,83 @@ function deleteTopic(button) {
         }
     });
 }
+
+function postTopic() {
+    let id = document.getElementById('idInput').value;
+    let name = document.getElementById('nameInput').value;
+    let image = document.getElementById('imageInput').value;
+    let description = document.getElementById('motaInput').value;
+
+    let insertTopic = {}
+    if (id) {
+        insertTopic = {
+            id: id,
+            name: name,
+            image: image,
+            description: description
+        };
+    } else {
+        insertTopic = {
+            name: name,
+            image: image,
+            description: description
+        };
+    }
+    console.table(insertTopic)
+    axios.post('/topic-api/postTopic', insertTopic)
+        .then(response => {
+            if (!document.getElementById('idInput').value) {
+                if (response.data.success) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Thành công',
+                        text: 'Đã thêm chuyên đề.',
+                        timer: 2000,
+                        confirmButtonColor: '#3b71ca'
+                    }).then(() => {
+                        location.reload();
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Thêm chuyên đề thất bại!',
+                        text: 'Đã xảy ra lỗi khi thêm thông tin chuyên đề. Vui lòng thử lại sau.',
+                        confirmButtonColor: '#3b71ca'
+                    });
+                }
+            } else {
+                if (response.data.success) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Thành công',
+                        text: 'Đã cập nhật chuyên đề.',
+                        timer: 2000,
+                        confirmButtonColor: '#3b71ca'
+                    }).then(() => {
+                        location.reload();
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Cập nhật thông tin thất bại',
+                        text: 'Đã xảy ra lỗi khi cập nhật thông tin chuyên đề. Vui lòng thử lại sau.',
+                        confirmButtonColor: '#3b71ca'
+                    });
+                }
+            }
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Lỗi',
+                text: 'Đã xảy ra lỗi khi gửi yêu cầu. Vui lòng thử lại sau.',
+                confirmButtonColor: '#3b71ca'
+            });
+        });
+}
+
 function updateTopic(button) {
     document.getElementById('ex1-tab-1').classList.toggle('active')
     document.getElementById('ex1-tab-1').classList.toggle('ripple-surface-primary')
@@ -135,16 +144,13 @@ function updateTopic(button) {
         id: data[0],
         name: data[1],
         image: data[2],
-        description: data[3],
-
+        description: data[3]
     }
     fillDataToForm(topic)
 }
 
-
 fillDataToForm = (topic) => {
     console.log(topic.id)
-
 
     document.getElementById('idInput').value = topic.id;
     document.getElementById('idInput').focus();
@@ -154,5 +160,4 @@ fillDataToForm = (topic) => {
     document.getElementById('imageInput').focus();
     document.getElementById('motaInput').value = topic.description;
     document.getElementById('motaInput').focus();
-
 }
